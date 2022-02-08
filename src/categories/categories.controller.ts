1340722d57bd8ee4a4bb9e554b1f8c4b4245fe68
Alan <dittler.a@gmail.com>
2022-02-08 18:44:24 -0300

    Create task endpoint created

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { Category } from './schema/category.schema';
@@ -14,9 +14,10 @@ export class CategoriesController {
 
   @Post()
   createCategory(
+    @Res() res,
     @Body() createCategoryDto: CreateCategoryDto,
     //in the future add the connection with the User
   ): Promise<Category> {
-    return this.categoriesService.createCategory(createCategoryDto);
+    return this.categoriesService.createCategory(createCategoryDto, res);
   }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,5 +1,10 @@
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Category, CategoryDocument } from './schema/category.schema';
 import { CreateCategoryDto } from './dto/create-category.dto';
@@ -10,14 +15,55 @@ export class CategoriesService {
     @InjectModel(Category.name) private categoryModel: Model<CategoryDocument>,
   ) {}
 
-  async createCategory(
-    createCategoryDto: CreateCategoryDto,
-  ): Promise<Category> {
-    const category = new this.categoryModel(createCategoryDto);
-    return category.save();
-  }
-
   async getCategories(): Promise<Category[]> {
     return this.categoryModel.find();
+  }
+
+  async createCategory(
+    createCategoryDto: CreateCategoryDto,
+    res,
+  ): Promise<Category> {
+    const name = createCategoryDto.name.toUpperCase();
+    const categoryDB = await this.categoryModel.findOne({ name });
+
+    if (categoryDB && categoryDB.state) {
+      return res.status(400).json({
+        msg: `Category ${createCategoryDto.name} already exists`,
+      });
+    }
+
+    if (categoryDB) {
+      const { _id } = categoryDB;
+      try {
+        await this.categoryModel.findByIdAndUpdate(_id, { state: true });
+        return res.status(200).json({
+          msg: `Category ${createCategoryDto.name} is active again`,
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(400).json({
+          msg: 'An error occurred, check the console',
+        });
+      }
+    }
+
+    const data = {
+      name,
+    };
+
+    const category = new this.categoryModel(data);
+
+    try {
+      await category.save();
+      res.status(200).json({
+        msg: 'category created',
+        category,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({
+        msg: 'An error occurred. please check the logs',
+      });
+    }
   }
 }
